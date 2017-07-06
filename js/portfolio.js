b07eae6154912b6b09b176261d16f6d260a5f786
Adam J Batson <adamjbatson@gmail.com>
2017-07-06 11:16:00 -0400

    Fix portfolio accordion so that the click event is only triggered when clicking on the heading

--- js/portfolio.js
+++ js/portfolio.js
@@ -5,10 +5,12 @@ $(document).ready(function () {
     $(".pf-entry-wrap").each(function(i, obj) {
     	$(this).find(".pf-details-wrap").slideUp(400);
     	$(this).find(".pf-arrow").html(downArrow);
-    	$(this).on("click", function() {
-    		$(this).find(".pf-details-wrap").slideToggle(400);
-    		var arrow = $(this).find(".pf-arrow");
-    		arrow.text((arrow.text() == upArrow) ? downArrow : upArrow);
+    	$(this).find("h3").each(function(j, h3) {
+    		$(this).on("click", function() {
+    			$(this).siblings(".pf-details-wrap").slideToggle(400);
+	    		var arrow = $(this).siblings(".pf-arrow");
+	    		arrow.text((arrow.text() == upArrow) ? downArrow : upArrow);
+    		});
     	});
     });
 });

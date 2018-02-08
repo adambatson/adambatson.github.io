37eb293d8165ee420ff96f3c9ba26f1a5fece23f
Adam J Batson <adamjbatson@gmail.com>
2018-02-07 20:37:47 -0500

    Major rehaul of contact form, basic email working

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,7 +1,18 @@
 $(document).ready(function() {
 
-	$(".dropdown").on('click', function() {
-		$(this).next(".dropdownContent").toggle();
+	$("#contactSubmit").on('click', function(e) {
+		e.preventDefault();
+		$.ajax({
+			dataType: 'jsonp',
+			url: "http://getsimpleform.com/messages/ajax?form_api_token=f67041d6f71c5c71a27b72256dbae882",
+			data: {
+				name: $(".name").val(),
+				email: $(".email").val(),
+				message: $(".message").val()
+			},
+			success: function(res) {
+				alert("Thank you for contacting me, I'll be in touch shortly!");
+			}
+		});
 	});
-
 });
